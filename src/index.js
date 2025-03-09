97578ae59e04d275773890b6a65b1cbce7ba3730
Chirath <chirathtimodh@gmail.com>
2025-03-09 12:41:16 +0530

    Change port to 8000

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = require("express")();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.listen(3000, () => {
+app.listen(8000, () => {
   console.log("Server is ready!");
 });
 
